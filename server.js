0381f9a40bd8e0e363f6f74c2552a2760ff3dba2
mariam <mariamshahathamada@gmail.com>
2026-07-18 19:52:54 +0300

    added  global error handling and 404 middlewares

--- server.js
+++ server.js
@@ -18,17 +18,29 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
 
-
-app.get('/', (req, res) => {
-    res.json({
-        success: true,
-        message: "Welcome to TechMaster E-Commerce API (Phase 1 / Sprint 1) - All Routes Registered!"
+app.use((req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: `Can't find ${req.url} on this server!`
     });
 });
 
 
-const PORT = process.env.PORT || 5000;
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid JSON format in request body"
+        });
+    }
 
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        success: false,
+        message: err.message || "Internal Server Error"
+    });
+});
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running in development mode on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
